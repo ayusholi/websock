91fc57a11cb99e6cea7b7d4875a5e7af874d5588
Ayush Oli <ayush.oli@hazesoft.co>
2022-04-11 18:25:40 +0545

    First Commit to deploy

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', function connection(ws) {
+    console.log("A new Client Connected");
+    ws.send('Welcome New Client');
+    ws.on('message', function incoming(message, isBinary) {
+        console.log('received: %s', message);
+
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message, { binary: isBinary })
+            }
+        });
+    });
+});
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+server.listen(3000, () => console.log('Listening on Port: 3000'))
