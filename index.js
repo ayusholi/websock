fdd8a32c9e676a4cb404ea966d18f9dc8eaa432e
Ayush Oli <ayush.oli@hazesoft.co>
2022-04-12 21:18:02 +0545

    Update on Index

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
+const express = require("express");
+const Websocket = require('ws');
+const app = express();
+const fs = require('fs');
 
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server: server });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const wss = new Websocket.Server({ port: 8081 });
+
+wss.on('connection', (ws) => {
+    console.log('One client connected');
+    ws.on("message", msg => {
+        ws.send(`msg is = ${msg}`);
+    });
+})
 
 wss.on('connection', function connection(ws) {
     console.log("A new Client Connected");
-    ws.send('Welcome New Client');
+    // ws.send('Welcome New Client');
     ws.on('message', function incoming(message, isBinary) {
         console.log('received: %s', message);
 
@@ -21,7 +31,26 @@ wss.on('connection', function connection(ws) {
 
 
 app.get('/new-relic-notify', (req, res) => {
-    ws.send(req.body);
+    console.log(req.query);
+    // Broadcast URL to connected ws clients
+    wss.clients.forEach((client) => {
+        // Check that connect are open and still alive to avoid socket error
+        if (client.readyState === Websocket.OPEN) {
+            var requestdata;
+            const filedata = fs.readFile('./var.json', (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                requestdata = data.toString();
+                // let response = req.query;
+                let response = requestdata;
+                client.send(JSON.stringify(response));
+            });
+        }
+    });
+
+    res.status(200).send({ message: "Notification sent to websockets" });
 });
 
-server.listen(3000, () => console.log('Listening on Port: 3000'))
+app.listen(3000, () => console.log('Listening on Port: 3000'))
